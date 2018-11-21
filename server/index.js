a70e175585469a1f103d612a454c5c0d4cfad9a5
Adrian Durran <adrian@intellidroid.eu>
2018-11-21 22:01:12 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,10 +4,18 @@ const morgan = require('morgan');
 
 require('dotenv').config({ path: 'variables.env' });
 
+const Recipe = require('./models/Recipe');
+const User = require('./models/User');
+
 mongoose
   .connect(
     process.env.MONGO_URI,
-    { useNewUrlParser: true, poolSize: 10 }
+    {
+      useNewUrlParser: true,
+      poolSize: 10,
+      useFindAndModify: false,
+      useCreateIndex: true
+    }
   )
   .then(() => console.log('Database connected'))
   .catch((error) => console.error('Unable to connect to database', error));
