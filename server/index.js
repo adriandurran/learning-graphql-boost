0ebce8cf593e3a6e71cb318606a1acd4b8a73a96
Adrian Durran <adrian@intellidroid.eu>
2018-11-21 21:56:02 +0000

    models

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const morgan = require('morgan');
+
+require('dotenv').config({ path: 'variables.env' });
+
+mongoose
+  .connect(
+    process.env.MONGO_URI,
+    { useNewUrlParser: true, poolSize: 10 }
+  )
+  .then(() => console.log('Database connected'))
+  .catch((error) => console.error('Unable to connect to database', error));
 
 const app = express();
 
